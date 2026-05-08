e9c9c7045bfa8ecddf06c5ed1641d5942d5197d6
Mohamed Hamed Qaddal <mohamed.h.qaddal@gmail.com>
2026-05-08 11:42:53 +0300

    feat: add signup screen UI

---
+++ app/src/main/java/com/mhq/fynecast/ui/screens/signup/SignupScreen.kt
@@ -0,0 +1,296 @@
+package com.mhq.fynecast.ui.screens.signup
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mhq.fynecast.ui.components.GlassyCard
+import com.mhq.fynecast.ui.theme.BabyBlue
+import com.mhq.fynecast.ui.theme.DuskBlue
+import com.mhq.fynecast.ui.theme.FyneCastTheme
+import com.mhq.fynecast.ui.theme.LilacBlue
+import com.mhq.fynecast.ui.theme.MidnightBlue
+import com.mhq.fynecast.ui.theme.NeonGreen
+
+@Composable
+fun SignupScreen(modifier: Modifier = Modifier) {
+
+    val focusRequester = remember { FocusRequester() }
+    var passwordVisible by remember { mutableStateOf(false)}
+    val gradientColors = listOf(NeonGreen, NeonGreen)
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .background(Brush.verticalGradient(listOf(MidnightBlue, DuskBlue, LilacBlue, BabyBlue)))
+    ) {
+        Box {
+            Text(
+                text = "FyneCast",
+                style = TextStyle(
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    drawStyle = Stroke(width = 5f)
+                )
+            )
+            Text(
+                text = "FyneCast",
+                style = TextStyle(
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold,
+                    brush = Brush.verticalGradient(colors = gradientColors)
+                )
+            )
+        }
+        GlassyCard(
+            modifier = Modifier.padding(36.dp)
+        ) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                label = { Text("Name") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "Name Icon",
+                        tint = Color.DarkGray
+                    )
+                },
+                placeholder = { Text("e.g. John Doe") },
+                shape = RoundedCornerShape(24.dp),
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color.White.copy(alpha = 0.03f),
+                    unfocusedContainerColor = Color.White.copy(alpha = 0.3f),
+                    focusedLabelColor = Color.Black,
+                    unfocusedLabelColor = Color.Black.copy(alpha = 0.7f)
+                )
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                label = { Text("Email") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email Icon",
+                        tint = Color.DarkGray
+                    )
+                },
+                placeholder = { Text("e.g. johndoe@gmail.com") },
+                shape = RoundedCornerShape(24.dp),
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color.White.copy(alpha = 0.3f),
+                    unfocusedContainerColor = Color.White.copy(alpha = 0.3f),
+                    focusedLabelColor = Color.Black,
+                    unfocusedLabelColor = Color.Black.copy(alpha = 0.7f)
+                )
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                label = { Text("Password") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Password Icon",
+                        tint = Color.DarkGray
+                    )
+                },
+                trailingIcon = {
+                    val image = if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
+                    val description = if (passwordVisible) "Hide password" else "Show password"
+
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(imageVector = image, contentDescription = description)
+                    }
+                },
+                placeholder = { Text("e.g. 12345") },
+                shape = RoundedCornerShape(24.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color.White.copy(alpha = 0.3f),
+                    unfocusedContainerColor = Color.White.copy(alpha = 0.3f),
+                    focusedLabelColor = Color.Black,
+                    unfocusedLabelColor = Color.Black.copy(alpha = 0.7f)
+                )
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                label = { Text("Confirm Password") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Password Icon",
+                        tint = Color.DarkGray
+                    )
+                },
+                trailingIcon = {
+                    val image = if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
+                    val description = if (passwordVisible) "Hide password" else "Show password"
+
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(imageVector = image, contentDescription = description)
+                    }
+                },
+                placeholder = { Text("e.g. 12345") },
+                shape = RoundedCornerShape(24.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = Color.White.copy(alpha = 0.3f),
+                    unfocusedContainerColor = Color.White.copy(alpha = 0.3f),
+                    focusedLabelColor = Color.Black,
+                    unfocusedLabelColor = Color.Black.copy(alpha = 0.7f)
+                )
+            )
+            //            Row(
+            //                verticalAlignment = Alignment.CenterVertically,
+            //                modifier = Modifier
+            //                    .padding(vertical = 4.dp)
+            //                    .fillMaxWidth()
+            //            ) {
+            //                CompositionLocalProvider(LocalMinimumInteractiveComponentSize provides Dp.Unspecified) {
+            //                    Checkbox(
+            //                        checked = false,
+            //                        onCheckedChange = {},
+            //                        modifier = Modifier.padding(end = 4.dp)
+            //                    )
+            //                }
+            //                Text(
+            //                    text = "Remember Me",
+            //                    fontSize = 12.sp,
+            //                    fontWeight = FontWeight.Bold,
+            //                    color = Color.White
+            //                )
+            //            }
+        }
+
+        Button(
+            onClick = {},
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MidnightBlue,
+                contentColor = Color.White
+            ),
+            modifier = Modifier
+                .padding(horizontal = 36.dp, vertical = 16.dp)
+                .height(56.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Sign up",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = NeonGreen
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 36.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+//            SocialMediaButton(
+//                text = "Sign in with Google",
+//                iconRes = R.drawable.ic_google_primary_light,
+//                backgroundColor = Color.White,
+//                contentColor = Color(0xFF1F1F1F),
+//                borderColor = Color(0xFF747775),
+//                onClick = { /* Handle Google Login */ }
+//            )
+//            SocialMediaButton(
+//                text = "Sign in with Facebook",
+//                iconRes = R.drawable.ic_facebook_primary_light,
+//                backgroundColor = Color(0xFF1877F2),
+//                contentColor = Color.White,
+//                onClick = { /* Handle Facebook Login */ }
+//            )
+            Row(){
+                Text(
+                    text = "Already have an account?",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = NeonGreen,
+                    modifier = Modifier
+                        .padding(4.dp)
+                )
+                Text(
+                    text = "Login",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MidnightBlue,
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clickable { /* Handle click */ }
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignupScreenScreenPreview() {
+    FyneCastTheme() {
+        SignupScreen()
+    }
+}
