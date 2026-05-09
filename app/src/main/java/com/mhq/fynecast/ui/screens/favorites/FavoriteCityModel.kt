135bd78ce8ea3279fdc6bbf2e0656225c34cb3ed
Mohamed Hamed Qaddal <mohamed.h.qaddal@gmail.com>
2026-05-09 18:07:59 +0300

    feat: add favorites, alerts and settings screens

---
+++ app/src/main/java/com/mhq/fynecast/ui/screens/favorites/FavoriteCityModel.kt
@@ -0,0 +1,7 @@
+package com.mhq.fynecast.ui.screens.favorites
+
+data class FavoriteCityModel (
+    var favoriteCityName: String,
+    var favoriteCityLatitude: Double,
+    var favoriteCityLongitude: Double
+)
