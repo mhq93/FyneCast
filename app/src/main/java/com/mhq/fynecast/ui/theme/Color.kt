c560b596e262d0428c679e14db5cd587e04685ff
Mohamed Hamed Qaddal <mohamed.h.qaddal@gmail.com>
2026-05-09 18:06:39 +0300

    feat: update app color palette

--- app/src/main/java/com/mhq/fynecast/ui/theme/Color.kt
+++ app/src/main/java/com/mhq/fynecast/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DeepBlue = Color(color = 0xFF88BDF2)
+val HappyBlue = Color(color = 0xFF02fef7)
+val LightBlue = Color(color = 0xFFBDDDFC)
+
+
+val MidnightBlue = Color(color = 0xFF001D39)
+val DuskBlue = Color(color = 0xFF0A4174)
+val LilacBlue = Color(color = 0xFF49769F)
+val BabyBlue = Color(color = 0xFF7BBDE8)
+
+val NeonGreen = Color(color = 0xFF2CFF05)
