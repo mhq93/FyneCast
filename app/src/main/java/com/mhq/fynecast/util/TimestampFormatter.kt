ab4d5b2e1dbdeaeed2c603b947027fd02ece438b
Mohamed Hamed Qaddal <mohamed.h.qaddal@gmail.com>
2026-06-09 19:37:07 +0300

    feat: add TimestampFormatter utility

---
+++ app/src/main/java/com/mhq/fynecast/util/TimestampFormatter.kt
@@ -0,0 +1,35 @@
+package com.mhq.fynecast.util
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+
+object TimestampFormatter {
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getFormattedHour(rawDateTime: String): String {
+        return try {
+            val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+            val dateTime = LocalDateTime.parse(rawDateTime, inputFormatter)
+            val outputFormatter = DateTimeFormatter.ofPattern("h a", Locale.US)
+            dateTime.format(outputFormatter)
+        } catch (e: Exception) {
+            rawDateTime
+        }
+    }
+
+    fun getFormattedDateAndTime(dateString: String): Pair<String, String> {
+        return try {
+            val sdfInput = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val date = sdfInput.parse(dateString) ?: return Pair("N/A", "N/A")
+            val dayName = SimpleDateFormat("EEE", Locale.getDefault()).format(date)
+            val monthDay = SimpleDateFormat("MMM d", Locale.getDefault()).format(date)
+            Pair(dayName, monthDay)
+        } catch (e: Exception) {
+            Pair("N/A", "N/A")
+        }
+    }
+}
